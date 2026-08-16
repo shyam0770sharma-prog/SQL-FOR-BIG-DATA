0170955623cddd22587645856e4466c797055099
SHREE KRISHAN SHARMA <shyam0770sharma@gmail.com>
2026-08-16 19:41:32 +0530

    Add files via upload

--- SQL.cte.sql
+++ SQL.cte.sql
@@ -83,13 +83,52 @@ WITH RECURSIVE cte AS (
 )
 SELECT * FROM cte;
 
+-- Q1. Find employees who have at least one employee reporting directly to them.
+select * from employees;
+select * from employees as e1 
+where exists 
+(select 1 from employees as e2 
+where e2.managerid = e1.employeeid);
 
+SELECT *
+FROM employees AS e1
+WHERE EXISTS (
+    SELECT 1
+    FROM employees AS e2
+    WHERE e2.managerid = e1.employeeid
+);
 
+-- Q2. Find employees who do not have any employee reporting directly to them.
+select * from employees as e1 
+where not exists(
+select 1 
+from employees as e2 
+where e2.managerid = e1.employeeid);
+-- Q3. Using a recursive CTE, display each employee's complete management 
+-- hierarchy from the CEO to that employee.
 
+select * from employees;
+with recursive cte as (
+select employeeid,name,name as levels from employees where managerid is null
+union all
+select e.employeeid,e.name,concat(cte.levels,'->',e.name) from 
+employees as e join cte on e.managerid=cte.employeeid where e.employeeid < 5)
+select * from cte;
 
+with recursive cte as (
+select employeeid,name,CAST(employeeid AS CHAR(100)) as levels from employees where managerid is null
+union all
+select e.employeeid,e.name,concat(cte.levels,'->',e.employeeid) from 
+employees as e join cte on e.managerid=cte.employeeid )
+select * from cte;
+-- Q4. Using a window function, assign a row number to employees within each manager,
+-- ordered by employeeid.
 
+select *, row_number() over(partition by managerid order by employeeid) from employees;
 
-
+-- Q5. Using a window function, calculate the number of employees reporting to each manager,
+-- while still displaying every employee.
+ select *, count(employeeid) over(partition by managerid) from employees;
 
 
 
