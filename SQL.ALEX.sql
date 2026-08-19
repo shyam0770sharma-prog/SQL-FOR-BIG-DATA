cc0df3aadadbf0faad9c9ac400eab4444db70377
SHREE KRISHAN SHARMA <shyam0770sharma@gmail.com>
2026-08-20 00:17:08 +0530

    Add files via upload

--- SQL.ALEX.sql
+++ SQL.ALEX.sql
@@ -399,7 +399,63 @@ count(age)
 from employee_demographics
 group by gender ) as agg_table
 ;
+USE `Parks_and_Recreation`;
+-- WINDOW FUNCTION
 
+select gender,avg(salary) as avg_salary
+from employee_demographics as dem
+join employee_salary as sal
+  on dem.employee_id = sal.employee_id
+group by gender;
+
+
+select gender,avg(salary) over()
+from employee_demographics as dem
+join employee_salary as sal
+  on dem.employee_id = sal.employee_id
+;
+
+select gender,avg(salary) over(partition by gender)
+from employee_demographics as dem
+join employee_salary as sal
+  on dem.employee_id = sal.employee_id
+;
+select dem.first_name,dem.last_name, gender,avg(salary) over(partition by gender)
+from employee_demographics as dem
+join employee_salary as sal
+  on dem.employee_id = sal.employee_id
+;
+
+select dem.first_name,dem.last_name, gender,salary,
+sum(salary) over(partition by gender order by dem.employee_id ) as Rolling_total
+from employee_demographics as dem
+join employee_salary as sal
+  on dem.employee_id = sal.employee_id
+;
+
+select dem.employee_id, dem.first_name,dem.last_name, gender,salary,
+row_number() over( partition by gender order by salary desc)
+from employee_demographics as dem
+join employee_salary as sal
+  on dem.employee_id = sal.employee_id
+;
+
+select dem.employee_id, dem.first_name,dem.last_name, gender,salary,
+row_number() over( partition by gender order by salary desc) as row_num,
+rank()over( partition by gender order by salary desc) as rank_num
+from employee_demographics as dem
+join employee_salary as sal
+  on dem.employee_id = sal.employee_id
+;
+
+select dem.employee_id, dem.first_name,dem.last_name, gender,salary,
+row_number() over( partition by gender order by salary desc) as row_num,
+rank()over( partition by gender order by salary desc) as rank_num,
+ dense_rank()over( partition by gender order by salary desc) as dense_rank_num
+from employee_demographics as dem
+join employee_salary as sal
+  on dem.employee_id = sal.employee_id
+;
 
 
 
