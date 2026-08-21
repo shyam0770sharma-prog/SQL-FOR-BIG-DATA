2622f4d8822373d2bfb0bf0c7e99cf6b90a93618
SHREE KRISHAN SHARMA <shyam0770sharma@gmail.com>
2026-08-21 18:59:03 +0530

    Add files via upload

--- SQL.ALEX.sql
+++ SQL.ALEX.sql
@@ -559,6 +559,45 @@ DELIMITER ;
 
 CALL large_salaries5(1); 
 
+USE `Parks_and_Recreation`;
+DROP TABLE IF EXISTS `Parks_and_Recreation`;
+-- Triggers and Events
+
+select * 
+from employee_demographics;
+delimiter $$
+select *
+from employee_salary;
+create trigger emloyee_insert
+	after insert on employee_salary
+    for each row
+begin
+	insert into employee_demographics (employee_id,first_name,last_name)
+    values (new.employee_id,new.first_name,new.last_name);
+end $$
+delimiter $$;
+
+insert into employee_salary (employee_id, first_name,last_name,occupation,
+salary,dept_id)
+values(13,'shyam-shyam','hero','hero-work',10000000,null);
+
+
+-- EVENTS
+select * 
+from employee_demographics;
+
+delimiter $$
+create event  delete_retirees
+on schedule every 30 second
+do 
+begin
+	delete
+    from employee_demographics
+    where age>= 60;
+end $$
+delimiter ;   
+
+show variables like 'event%';
 
 
 
